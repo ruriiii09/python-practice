e918dc3c1f63dbdf181c0ba91a06799786123de0
Rur <73644232+ruriiii09@users.noreply.github.com>
2026-03-11 21:27:18 +0900

    Update mosh_re.py

--- mosh_re.py
+++ mosh_re.py
@@ -87,7 +87,7 @@ def get_latest_mid_data():
 def get_worksheet():
     # --- 1. 接続の準備（これは共通） ---
     scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    creds = Credentials.from_service_account_file('secrets.json', scopes=scope)
+    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
     client = gspread.authorize(creds)
     
    # スプレッドシートとワークシートを開く
